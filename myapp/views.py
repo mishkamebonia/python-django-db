58647626f3a2f20d1c2e7f41622307357385aa44
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-17 01:40:50 +0400

    removed db and imported

--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-from .models import Employee, Stack
-
-mock_db = [
-    {'id': 1, 'name': 'John', 'surname': 'Doe', 'stack': 'Python', 'team_lead': 'David'},
-    {'id': 2, 'name': 'Jane', 'surname': 'Smith', 'stack': 'Java', 'team_lead': 'Joana'},
-    {'id': 3, 'name': 'Mike', 'surname': 'Johnson', 'stack': 'JavaScript', 'team_lead': 'Smith'},
-    {'id': 4, 'name': 'Emily', 'surname': 'Williams', 'stack': 'C#', 'team_lead': 'Sarah'},
-    {'id': 5, 'name': 'Alex', 'surname': 'Brown', 'stack': 'Ruby', 'team_lead': 'Philip'},
-    {'id': 6, 'name': 'Sarah', 'surname': 'Miller', 'stack': 'Python', 'team_lead': 'David'},
-    {'id': 7, 'name': 'Daniel', 'surname': 'Jones', 'stack': 'Java', 'team_lead': 'Joana'},
-    {'id': 8, 'name': 'Sophia', 'surname': 'Davis', 'stack': 'JavaScript', 'team_lead': 'Smith'},
-    {'id': 9, 'name': 'Matthew', 'surname': 'Garcia', 'stack': 'C#', 'team_lead': 'Sarah'},
-    {'id': 10, 'name': 'Olivia', 'surname': 'Martinez', 'stack': 'Ruby', 'team_lead': 'Philip'},
-]
+from .db.mock_data import mock_db
 
 def employees(request):
     employees = []
@@ -23,24 +10,29 @@ def employees(request):
             'name': employee['name'],
             'surname': employee['surname'],
             'stack': employee['stack'],
-            'team_lead': employee['team_lead']
+            'team_lead': employee['team_lead'],
         })
 
     return render(request, 'employees.html', {'employees': employees})
-
 
 def stacks(request):
     stacks_set = set()
     stacks = []
 
     for employee in mock_db:
-        if employee['stack'] not in stacks_set:
-            stacks_set.add(employee['stack'])
-            stacks.append({
-                'name': employee['stack']
-            })
+        stack = employee['stack']
+        if stack not in stacks_set:
+            stacks_set.add(stack)
+            stack_users = [{'name': employee['name'], 'surname': employee['surname']}]
+            stacks.append({'stack': stack, 'users': stack_users})
+        else:
+            for stack_data in stacks:
+                if stack_data['stack'] == stack:
+                    stack_data['users'].append({'name': employee['name'], 'surname': employee['surname']})
+                    break
 
     return render(request, 'stacks.html', {'stacks': stacks})
+
 
 def team_leads(request):
     team_leads_set = set()
@@ -49,12 +41,17 @@ def team_leads(request):
     for employee in mock_db:
         if employee['team_lead'] not in team_leads_set:
             team_leads_set.add(employee['team_lead'])
+            users = []
+            for emp in mock_db:
+                if emp['team_lead'] == employee['team_lead']:
+                    users.append({'name': emp['name'], 'surname': emp['surname']})
             team_leads.append({
                 'name': employee['team_lead'], 
-                'stack': employee['stack']
+                'users': users
             })
 
     return render(request, 'team_leads.html', {'team_leads': team_leads})
+
 
 def home(request):
     return render(request, 'home.html')
